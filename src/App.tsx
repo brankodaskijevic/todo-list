a66865d997794761281ebb3427a86573d9b94582
Branko Daskijević <branko.dask@gmail.com>
2022-02-06 22:23:13 +0100

    todo-list: changing stiches with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <Button color={'violet'}>My button</Button>
+      <Button>Click me</Button>
     </div>
   )
 }
--- src/components/global/button/Button.tsx
+++ src/components/global/button/Button.tsx
@@ -1,22 +1,18 @@
-import { styled } from '@stitches/react'
+import styled from 'styled-components'
 
-const Button = styled('button', {
-  backgroundColor: 'Gainsboro',
-  borderRadius: '9999px',
-  border: 'none',
-  cursor: 'pointer',
-  fontWeight: 'bold',
-  fontSize: '14px',
-  padding: '10px 15px',
-  '&:hover': {
-    backgroundColor: 'LightGray'
-  },
-  variants: {
-    color: {
-      violet: { backgroundColor: 'blueviolet' },
-      gray: { backgroundColor: 'gainsboro' },
-    }
+const Button = styled.button`
+  padding: 0.5em 1em;
+  font-size: 1.2em;
+  border-radius: 999px;
+  border: none;
+  font-weight: bold;
+  background-color: #219F94;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e3e3e3;
   }
-})
+`
 
 export default Button

035d26e1912ef082d42585e653bce416393b66a6
Branko Daskijević <branko.dask@gmail.com>
2022-02-11 17:51:44 +0100

    todo-list: changing button padding

--- src/components/global/button/Button.tsx
+++ src/components/global/button/Button.tsx
@@ -4,7 +4,7 @@ export const Button = styled.button`
   background-color: ${({ color }) => color ? 'rgb(224, 49, 49)' : 'rgb(25, 113, 194)'};
   color: white;
   font-weight: bold;
-  padding: 0.6em 1.2em;
+  padding: 0.4em 0.8em;
   margin: 0.4em;
   border: none;
   border-radius: 6px;

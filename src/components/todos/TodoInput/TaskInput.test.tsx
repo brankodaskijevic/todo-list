d6facd0069e44ba9b8c66eed2b7acc4c2ff48468
Branko Daskijević <branko.dask@gmail.com>
2022-02-18 19:16:14 +0100

    todo-list: started working on todo input test

---
+++ src/components/todos/TodoInput/TaskInput.test.tsx
@@ -0,0 +1,44 @@
+import { cleanup, render, RenderResult } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { v4 as uuidv4 } from 'uuid'
+import TaskInput from './TaskInput'
+import { Todo as Task } from '../../../types';
+import { TodoProps } from './interfaces';
+
+describe('TodoInput', () => {
+  let renderResult: RenderResult
+
+  const onSetTasks: jest.Mock = jest.fn()
+  const todos: Array<Task> = [
+    { id: uuidv4(), text: 'test todo 1', complete: false, priority: 'HIGH' },
+  ]
+
+  const todoProps: TodoProps = {
+    todos,
+    onSetTasks
+  }
+
+  beforeEach(() => {
+    renderResult = render(<TaskInput {...todoProps} />)
+  })
+
+  afterAll(() => {
+    cleanup()
+  })
+
+  it('renders Todo component with right properties', () => {
+    const todoText: Element = renderResult.getByText('test todo 1')
+    const todoPriority: Element = renderResult.getByText('HIGH')
+
+    expect(todoText).toBeInTheDocument()
+    expect(todoPriority).toBeInTheDocument()
+  })
+
+  it('should delete item when user clicks on X button', () => {
+    const deleteButton: Element = renderResult.getByText('X')
+
+    userEvent.click(deleteButton)
+
+    expect(onSetTasks).toHaveBeenCalled()
+  })
+})
